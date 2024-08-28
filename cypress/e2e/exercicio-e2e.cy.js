907935fb0cd21e6d0f7f4eab68860f58e0f6272f
Joycefermorito <joycefmtim@gmail.com>
2024-08-28 14:38:10 -0300

    Descrição das alterações realizadas no cenário de teste

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,22 +1,29 @@
-/// <reference types="cypress" />
+///<reference types="cypress" />
+import produtosPage from "../support/page_objects/produtos.page";
 
-context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-  /*  Como cliente 
-      Quero acessar a Loja EBAC 
-      Para fazer um pedido de 4 produtos 
-      Fazendo a escolha dos produtos
-      Adicionando ao carrinho
-      Preenchendo todas opções no checkout
-      E validando minha compra ao final */
+describe('Fluxo de Compra na Loja EBAC', () => {
 
-  beforeEach(() => {
-      cy.visit('/')
-  });
+    beforeEach(() => {
+        cy.visit('minha-conta')
+        cy.fixture('perfil').then(login => {
+            cy.login(login.usuario, login.senha)
+        })
+    });
+    beforeEach(() => {
+        produtosPage.visitarUrl()
+    });
 
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
-  });
+    it('deve adicionar quatro produtos ao carrinho e realizar o checkout', () => {
+        cy.fixture('produtos').then(dados => {
+            dados.forEach(produto => {
+                produtosPage.buscarProduto(produto.nomeProduto)
+                produtosPage.addProdutoCarrinho(produto.tamanho, produto.cor, produto.quantidade)
+                cy.get('.woocommerce-message').should('contain', produto.nomeProduto)
+            });
 
-
-})
+            cy.visit('checkout'); 
+            cy.checkout('Aluno', 'Teste',  'Ebac', 'Ebac Rua dos Bobos', '0', 'São Paulo', '00000-000', '(99) 99999-9999', 'aluno_ebac@teste.com', 'Casa de chão', 'Concordo com os termos');
+            cy.get('.woocommerce-notice', { timeout: 5000 }).should('contain', 'Obrigado. Seu pedido foi recebido.')
+        });
+    });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,26 @@
 Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
+    cy.get('#password').type(senha, { log: false })
     cy.get('.woocommerce-form > .button').click()
 });
+Cypress.Commands.add('checkout', (nome, sobrenome, endereco, empresa, numero, cidade, cep, telefone, email, informacao, termo) => {
+    cy.get('#billing_first_name').clear().type(nome);
+    cy.get('#billing_last_name').clear().type(sobrenome);
+    cy.get('#billing_company').clear().type(empresa);
+    cy.get('#billing_address_1').clear().type(endereco);
+    cy.get('#billing_address_2').clear().type(numero);
+    cy.get('#billing_city').clear().type(cidade);
+    cy.get('#billing_postcode').clear().type(cep);
+    cy.get('#billing_phone').clear().type(telefone);
+    cy.get('#billing_email').clear().type(email); 1  
+    cy.get('#order_comments').type(informacao)
+    cy.get('#terms').type(termo).click()
+    cy.get('#place_order').click()
+    
+    
+
+
+
+}); 
+
 
